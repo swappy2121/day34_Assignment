8beca9767a31517c132397a672885e342e739b24
swapnil <sopiswarrinku77@gmail.com>
2022-06-02 11:28:56 -0700

    Added : retrieved all employees present into a table

---
+++ RetrievingAllEmployeesUC5.java
@@ -0,0 +1,27 @@
+package com.Bridgelabz;
+
+import java.sql.*;
+
+public class RetrievingAllEmployeesUC5 {
+    public static void main(String[] args) throws SQLException {
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/employeepayroll_service?useSSL=false";
+        String userName = "root";
+        String password = "sopi@1234";
+        String query = "select * from detailsofemployees";
+        Connection connection;
+
+        try {
+            connection = DriverManager.getConnection(jdbcUrl, userName, password);
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet result = statement.executeQuery();
+            System.out.println("Name of employees : ");
+            while (result.next()) {
+                String name = result.getString("name");
+                System.out.print(name + " \n");
+            }
+        } catch (SQLException exec) {
+            exec.printStackTrace();
+        }
+    }
+}
